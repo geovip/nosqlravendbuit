ef85771a9af9575dc27effbe46de2e9369c94d9c
huyuit@gmail.com <huyuit@gmail.com@30cfe87c-cee9-b051-0c98-8c61824b0c8f>
2012-10-06 04:13:00 +0000

    new domain for new detail design version 2

---
+++ UIT.NoSQL.Core/NewDomain/Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UIT.NoSQL.Core.NewDomain
+{
+    public class Group
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public List<DenormalizedTopic> ListTopic { get; set; }
+        public List<UserGroup> ListUserGroup { get; set; }
+    }
+
+    public class DenormalizedGroup
+    {
+        public string GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ UIT.NoSQL.Core/NewDomain/Topic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UIT.NoSQL.Core.NewDomain
+{
+    public class Topic
+    {
+        public string TopicId { get; set; }
+        public string TopicName { get; set; }
+        public string Content { get; set; }
+        public DenormalizedUser CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public UInt32 NumberOfView { get; set; }
+        public UInt32 NumberOfComment { get; set; }
+
+        public List<Comment> ListComment { get; set; }
+
+        public Topic()
+        {
+            ListComment = new List<Comment>();
+        }
+
+        public UInt32 GetNumberOfComment()
+        { 
+            return (UInt32)ListComment.Count();
+        }
+
+    }
+
+    public class DenormalizedTopic
+    {
+        public string TopicId { get; set; }
+        public string TopicName { get; set; }
+        public DenormalizedUser CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public UInt32 NumberOfView { get; set; }
+        public UInt32 NumberOfComment { get; set; }
+    }
+
+    public class Comment
+    {
+        public string CommentId { get; set; }
+        public string Content { get; set; }
+        public DenormalizedUser CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
