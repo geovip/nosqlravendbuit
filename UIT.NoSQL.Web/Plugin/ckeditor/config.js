1d18cde4b1fd11aa2055dd264d01227867909460
huyuit@gmail.com <huyuit@gmail.com@30cfe87c-cee9-b051-0c98-8c61824b0c8f>
2012-12-18 09:40:52 +0000

    config editor standard

--- UIT.NoSQL.Web/Plugin/ckeditor/config.js
+++ UIT.NoSQL.Web/Plugin/ckeditor/config.js
@@ -8,4 +8,16 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.toolbar = 'MyToolbar';
+ 
+	config.toolbar_MyToolbar =
+	[
+		{ name: 'styles', items : [ 'Font','FontSize' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','-','RemoveFormat' ] },
+		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+		{ name: 'links', items : [ 'Link','Unlink' ] },
+		{ name: 'insert', items : [ 'Image','Table','HorizontalRule','PageBreak'] }
+
+	];
 };
