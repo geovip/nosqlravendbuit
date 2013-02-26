a4badc0a0569ce6df911efbf954bb4bd7c6acba8
duongthandan@gmail.com <duongthandan@gmail.com@30cfe87c-cee9-b051-0c98-8c61824b0c8f>
2013-02-26 16:36:47 +0000

    update search function

--- trunk/UIT.NoSQL.Core/IService/IGroupService.cs
+++ trunk/UIT.NoSQL.Core/IService/IGroupService.cs
@@ -17,8 +17,7 @@ namespace UIT.NoSQL.Core.IService
         GroupObject Load(string id);
         GroupObject LoadWithUser(string groupID, out List<UserObject> listUser, out List<UserGroupObject> listUserGroup);
         GroupObject LoadWithUser(string id);
-        List<GroupObject> Search(string searchStr, int take, out int totalResult);
-        List<GroupObject> Search(string searchStr, int skip, int take);
+        List<GroupObject> Search(string searchStr, out int totalResult);
         List<GroupObject> LoadList(string[] arrId);
         bool SendEmail(List<UserGroupObject> listUser, string subject, string body);
     }
